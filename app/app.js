37e160c4ca91ae2be603a906a00098c47b8623d2
UDAD247 <udadeveryday@gmail.com>
2023-10-27 22:08:43 -0600

    added_some_effects

--- app/app.js
+++ app/app.js
@@ -1,21 +1,14 @@
-// Get references to the "Pricing" link, the About section, and the About header
+// Get references to the "Pricing" link, the About section, and the About header section
 const pricingLink = document.getElementById("aboutLink"); // Assuming "aboutLink" is your Pricing link
 const aboutSection = document.querySelector(".about"); // Select the element with class "about"
-const aboutHeader = document.querySelector(".about_header"); // Select the element with class "about_header"
+const aboutHeader = document.querySelector(".about_header"); // Select the entire "about_header" section
 
-// Hide the "About" section initially
-aboutSection.style.display = "none";
-
-// Function to toggle the display of the "About" section
+// Function to toggle the "About" section with a smooth dropdown effect
 function toggleAboutSection() {
-  if (aboutSection.style.display === "none") {
-    aboutSection.style.display = "inline-block";
-  } else {
-    aboutSection.style.display = "none";
-  }
+  aboutSection.classList.toggle("active");
 }
 
-// Add click event listeners to both the "Pricing" link and the "About" header
+// Add click event listeners to both the "Pricing" link and the entire "About" header section
 pricingLink.addEventListener("click", (e) => {
   e.preventDefault();
   toggleAboutSection();
